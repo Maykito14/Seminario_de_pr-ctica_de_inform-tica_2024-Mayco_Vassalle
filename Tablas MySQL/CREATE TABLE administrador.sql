0ba96c25baa941f3c8551126d60fbd3e3e184360
Mayco Alexis Vassalle <120343897+Maykito14@users.noreply.github.com>
2024-11-17 16:11:33 -0300

    Delete Tablas MySQL directory

--- Tablas MySQL/CREATE TABLE administrador.sql
+++
@@ -1,8 +0,0 @@
-use sgtb;
-
-CREATE TABLE `sgtb`.`administrador` (
-  `administradorID` INT NOT NULL AUTO_INCREMENT,
-  `nombre` VARCHAR(45) NOT NULL,
-  `apellido` VARCHAR(45) NOT NULL,
-  `email` VARCHAR(45) NOT NULL,
-  PRIMARY KEY (`administradorID`));
--- Tablas MySQL/CREATE TABLE torneo.sql
+++
@@ -1,8 +0,0 @@
-use sgtb;
-
-CREATE TABLE `sgtb`.`torneo` (
-  `torneoID` INT NOT NULL AUTO_INCREMENT,
-  `nombre` VARCHAR(45) NOT NULL,
-  `fecha` DATETIME NOT NULL,
-  `estado` TINYINT NOT NULL,
-  PRIMARY KEY (`torneoID`));
